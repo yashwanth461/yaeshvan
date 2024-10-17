5487aee7b3c2a9b13da34dff5e782d19c2b6ef09
yashwanth461 <128785163+yashwanth461@users.noreply.github.com>
2024-10-17 12:02:47 +0000

    changes for file

--- vpc.tf
+++ vpc.tf
@@ -55,5 +55,6 @@ resource "aws_route_table_assosciation" "publicasso"{
 resource "aws_route" "route" {
   route_table_id = "aws_route_table_publicroute.id"
   destination_cidr_block = "0.0.0.0/0"
+  gateway_id = "aws_internet_gateway.my-gw.id"
 }
 
