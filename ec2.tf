9714d10578cd41332c5021f434f94aa730d56665
yashwanth461 <128785163+yashwanth461@users.noreply.github.com>
2024-10-17 12:35:48 +0000

    commit

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my-web" {
   ami="ami-078264b8ba71bc45e"
   instance_type = "t2.micro"
   tags = {
-    Name="terraforms"
+    Name="terraformss"
   }
   key_name = "jenkins"
  
